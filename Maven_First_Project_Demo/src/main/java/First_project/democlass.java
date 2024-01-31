646bee7b83f00d4aeb0daf5a4f32a6751453fe67
CxSeanC <81707723+CxSeanC@users.noreply.github.com>
2024-01-30 20:10:36 -0500

    Update democlass.java

--- Maven_First_Project_Demo/src/main/java/First_project/democlass.java
+++ Maven_First_Project_Demo/src/main/java/First_project/democlass.java
@@ -1,9 +1,38 @@
+// package First_project;
+
+// public class democlass {
+	
+// 	public String demo() {
+		
+// 		return "hello";
+// 	}
+
+// }
 package First_project;
 
-public class democlass {
-	
-	public String demo() {
-		return "hello";
-	}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class democlass {
+
+    public String demo(String userProvidedInput) {
+        String response = "";
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "user", "password");
+            Statement stmt = con.createStatement();
+            // Vulnerable to SQL Injection
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE name = '" + userProvidedInput + "'");
+
+            while (rs.next()) {
+                response += rs.getString(1);
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 }
+
